0a7625df919e6b69734bb8e8f29061575a91f4bc
Greg Tatum <tatum.creative@gmail.com>
2021-08-08 15:01:00 -0500

    Update references to the CLI

--- example/gen_all.sh
+++ example/gen_all.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-../target/debug/diplomat-tool js js/ js/docs/
+../target/debug/diplomat-tool js js/ --docs js/docs/
 ../target/debug/diplomat-tool c c/
-../target/debug/diplomat-tool cpp cpp/ cpp/docs/
+../target/debug/diplomat-tool cpp cpp/ --docs cpp/docs/
