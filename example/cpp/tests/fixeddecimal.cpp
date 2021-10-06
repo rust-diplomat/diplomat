a0ecaed9f26ac042e002e9d6d261d484e0c2bbf3
Manish Goregaokar <manishsmail@gmail.com>
2021-10-05 17:21:16 -0700

    Add assert macro

--- example/cpp/tests/fixeddecimal.cpp
+++ example/cpp/tests/fixeddecimal.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
 #include "../ICU4XFixedDecimalFormat.hpp"
+#include "assert.hpp"
 
 int main(int argc, char *argv[]) {
     ICU4XFixedDecimal fd = ICU4XFixedDecimal::new_(123);
 
-    if (fd.to_string().is_err()) {
-        std::cout<<"Failed to construct FixedDecimal"<<std::endl;
-        return 1;
-    }
+    assert("constructing FixedDecimal", !fd.to_string().is_err());
 
     std::string fd_out = fd.to_string().ok().value();
 
-    if (fd_out != "123") {
-        std::cout << "Expected 123, found " << fd_out << std::endl;
-        return 1;
-    }
-    
+    assert_eq("Stringifying FixedDecimal", fd_out, "123");
+
     fd.multiply_pow10(-1);
 
     fd_out = fd.to_string().ok().value();
-    if (fd_out != "12.3") {
-        std::cout << "Expected 12.3, found " << fd_out << std::endl;
-        return 1;
-    }
+
+    assert_eq("Multiplying FixedDecimal", fd_out, "12.3");
+
     std::string out;
 
     fd.to_string_to_writeable(out);
 
-    if (out != "12.3") {
-        std::cout << "Writeable: expected 12.3, found " << out << std::endl;
-        return 1;
-    }
+    assert_eq("Formatting FixedDecimal to Writeable", fd_out, "12.3");
 
     std::array<uint8_t, 2> bytes = {'e', 'n'};
     ICU4XLocale locale = ICU4XLocale::new_from_bytes(bytes);
@@ -40,16 +31,12 @@ int main(int argc, char *argv[]) {
     ICU4XDataProvider data_provider = ICU4XDataProvider::new_static();
 
     auto fdf = ICU4XFixedDecimalFormat::try_new(locale, data_provider, ICU4XFixedDecimalFormatOptions::default_());
-    if (!fdf.success) {
-        std::cout << "Failed to format fixed decimal" << std::endl;
-        return 1;
-    }
+
+    assert("Formatting FixedDecimal", fdf.success);
 
     out = fdf.fdf.value().format_write(fd);
 
-    if (out != "১২.৩") {
-        std::cout << "Expected ১২.৩, found " << out << std::endl;
-        return 1;
-    }
+    assert_eq("Formatting FixedDecimal", out, "১২.৩");
+
     std::cout << "Formatted value is " << out << std::endl;
 }
