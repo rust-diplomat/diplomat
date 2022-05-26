7d2ce282ddd5994f4acb6538aa82b52885da2bfe
Manish Goregaokar <manishsmail@gmail.com>
2022-05-26 10:45:32 -0700

    Move c headers to c/include

--- example/gen_all.sh
+++ example/gen_all.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ../target/debug/diplomat-tool js js/ --docs js/docs/ --docs-base-urls=*:https://unicode-org.github.io/icu4x-docs/doc/ 
-../target/debug/diplomat-tool c c/
+../target/debug/diplomat-tool c c/include
 ../target/debug/diplomat-tool cpp cpp/include --docs cpp/docs/ --docs-base-urls=*:https://unicode-org.github.io/icu4x-docs/doc/
