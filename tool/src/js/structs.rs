b35d977693c489f97a8cd5c61efe48f9ec4fa6ea
Quinn Okabayashi <qokabay1@swarthmore.edu>
2022-06-17 15:48:21 -0400

    Fix non-opaque struct expansion

--- tool/src/js/structs.rs
+++ tool/src/js/structs.rs
@@ -176,6 +176,18 @@ fn gen_method<W: fmt::Write>(
     let mut all_param_exprs = vec![];
     let mut post_stmts = vec![];
 
+    if let Some(ref self_param) = method.self_param {
+        gen_value_js_to_rust(
+            &ast::Ident::from("this"),
+            &self_param.to_typename(),
+            in_path,
+            env,
+            &mut pre_stmts,
+            &mut all_param_exprs,
+            &mut post_stmts,
+        );
+    }
+
     for p in method.params.iter() {
         gen_value_js_to_rust(
             &p.name,
@@ -201,10 +213,6 @@ fn gen_method<W: fmt::Write>(
     }
 
     let all_params_invocation = {
-        if method.self_param.is_some() {
-            all_param_exprs.insert(0, "this.underlying".to_string());
-        }
-
         if let Some(ref return_type) = method.return_type {
             if let ReturnTypeForm::Complex = return_type_form(return_type, in_path, env) {
                 all_param_exprs.insert(0, "diplomat_receive_buffer".to_string());
@@ -242,9 +250,9 @@ fn gen_method<W: fmt::Write>(
                             ValueIntoJs {
                                 value_expr: &invocation_expr,
                                 typ,
-                                in_path,
                                 borrows_self: borrowed_params.borrows_self(),
                                 borrowed_params: &borrowed_params.1[..],
+                                in_path,
                                 env,
                             }
                             .fmt(f)
