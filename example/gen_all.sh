03c2bc83782650ad6b4dc0f4b3283d579033d34d
Quinn Okabayashi <qokabay1@swarthmore.edu>
2022-07-15 11:40:22 -0700

    fix ci

--- example/gen_all.sh
+++ example/gen_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 ../target/debug/diplomat-tool js js/lib/api --docs js/lib/docs/ --docs-base-urls=*:https://unicode-org.github.io/icu4x-docs/doc/ 
-cp ../target/wasm32-unknown-unknown/debug/diplomat_example.wasm ./js/lib
+# cp ../target/wasm32-unknown-unknown/debug/diplomat_example.wasm ./js/lib
 ../target/debug/diplomat-tool c c/include
 ../target/debug/diplomat-tool cpp cpp/include --docs cpp/docs/ --docs-base-urls=*:https://unicode-org.github.io/icu4x-docs/doc/
