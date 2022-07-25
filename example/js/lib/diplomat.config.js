ad78eae9261e3e6394bd15aa3d76f860d1e60eca
Quinn Okabayashi <qokabay1@swarthmore.edu>
2022-07-25 14:48:39 -0700

    add `diplomat.config.js` file

---
+++ example/js/lib/diplomat.config.js
@@ -0,0 +1,3 @@
+export default {
+    wasm_path: new URL('./api/diplomat_example.wasm', import.meta.url),
+};
---
+++ feature_tests/js/diplomat.config.js
@@ -0,0 +1,3 @@
+export default {
+    wasm_path: new URL('./diplomat_feature_tests.wasm', import.meta.url),
+};
