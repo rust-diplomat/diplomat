28bb2b92c76d5f8496ebb677f5e9d5366d969690
Robert Bastian <robertbastian@users.noreply.github.com>
2022-10-06 23:07:05 +0200

    -cp (#268)

--- example/gen_all.sh
+++ example/gen_all.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
 ../target/debug/diplomat-tool js js/lib/api --docs js/lib/docs/ --docs-base-urls=*:https://unicode-org.github.io/icu4x-docs/doc/
-cp ../tool/src/js/wasm.mjs js/lib/api/diplomat-wasm.mjs
 ../target/debug/diplomat-tool c c/include
 ../target/debug/diplomat-tool cpp cpp/include --docs cpp/docs/ --docs-base-urls=*:https://unicode-org.github.io/icu4x-docs/doc/
