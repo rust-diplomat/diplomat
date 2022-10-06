81a93ad211a7e18e25442e5137f6357438b3fb9c
Robert Bastian <robertbastian@users.noreply.github.com>
2022-10-06 13:59:08 -0700

    -cp

--- feature_tests/gen_all.sh
+++ feature_tests/gen_all.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 ../target/debug/diplomat-tool js js/api --docs js/docs/
-cp ../tool/src/js/wasm.mjs js/api/diplomat-wasm.mjs
 ../target/debug/diplomat-tool c c/include
 ../target/debug/diplomat-tool cpp cpp/include --docs cpp/docs/
 ../target/debug/diplomat-tool dotnet dotnet/Lib/Generated/ -l dotnet/dotnet-interop-conf.toml
