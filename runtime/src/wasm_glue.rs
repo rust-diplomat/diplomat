bebf6f4b5e8235b06862e221d5fbc5e421650ef9
Robert Bastian <robertbastian@users.noreply.github.com>
2022-10-06 11:52:19 -0700

    wip

--- runtime/src/wasm_glue.rs
+++ runtime/src/wasm_glue.rs
@@ -1,70 +1,41 @@
-use std::ffi::CString;
-use std::io;
-use std::os::raw::c_char;
-
-#[cfg(debug_assertions)]
-use std::panic;
-
 // minimal WASM logger based on https://github.com/DeMille/wasm-glue
 extern "C" {
-    #[allow(dead_code)] // we want a consistent set of externs
-    fn trace_js(ptr: *const c_char);
-    #[allow(dead_code)]
-    fn warn_js(ptr: *const c_char);
-    #[allow(dead_code)]
-    fn log_js(ptr: *const c_char);
+    fn trace_js(ptr: *const u8, len: usize);
+    fn warn_js(ptr: *const u8, len: usize);
+    fn log_js(ptr: *const u8, len: usize);
 }
 
 /// Throw an exception.
-#[allow(dead_code)]
-pub fn console_trace(msg: &str) -> io::Result<()> {
-    console(msg, trace_js)
+pub fn console_trace(msg: &str) {
+    trace_js(str.as_ptr(), str.len());
 }
 
 /// Write a message to `console.warn`.
-#[allow(dead_code)]
-pub fn console_warn(msg: &str) -> io::Result<()> {
-    console(msg, warn_js)
+pub fn console_warn(msg: &str) {
+    warn_js(str.as_ptr(), str.len())
 }
 
 /// Write a message to `console.log`.
-#[allow(dead_code)]
-pub fn console_log(msg: &str) -> io::Result<()> {
-    console(msg, log_js)
-}
-
-/// Call a JavaScript function that takes a string.
-fn console(msg: &str, f: unsafe extern "C" fn(*const c_char)) -> io::Result<()> {
-    let cstring = CString::new(msg)?;
-    unsafe {
-        f(cstring.as_ptr());
-    }
-    Ok(())
-}
-
-/// Sets a custom panic hook, uses your JavaScript `trace` function
-#[cfg(debug_assertions)]
-fn set_panic_hook() {
-    panic::set_hook(Box::new(|info| {
-        let file = info.location().unwrap().file();
-        let line = info.location().unwrap().line();
-        let col = info.location().unwrap().column();
-
-        let msg = match info.payload().downcast_ref::<&'static str>() {
-            Some(s) => *s,
-            None => match info.payload().downcast_ref::<String>() {
-                Some(s) => &s[..],
-                None => "Box<Any>",
-            },
-        };
-
-        let err_info = format!("Panicked at '{}', {}:{}:{}", msg, file, line, col);
-        console_trace(&err_info).unwrap();
-    }));
+pub fn console_log(msg: &str) {
+    log_js(str.as_ptr(), str.len())
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn diplomat_init() {
     #[cfg(debug_assertions)]
-    set_panic_hook();
+    /// Sets a custom panic hook using `trace_js`, which by default crates a JS error
+    std::panic::set_hook(Box::new(|info| {
+        let file = info.location().unwrap().file();
+        let line = info.location().unwrap().line();
+        let col = info.location().unwrap().column();
+
+        let msg = match info.payload().downcast_ref::<&'static str>() {
+            Some(&s) => s,
+            None => match info.payload().downcast_ref::<String>() {
+                Some(s) => s.as_str(),
+                None => "Box<Any>",
+            },
+        };
+        console_trace(&format!("Panicked at '{msg}', {file}:{line}:{col}"));
+    }));
 }
