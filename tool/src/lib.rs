30cd7a19f50458f93a3298c65b00e7415d0b5df6
Manish Goregaokar <manishsmail@gmail.com>
2023-09-21 15:39:32 -0700

    note the lint that will help us

--- tool/src/lib.rs
+++ tool/src/lib.rs
@@ -1,3 +1,6 @@
+// Enable once https://github.com/rust-lang/rust/issues/89554 is stable
+// #![deny(non_exhaustive_omitted_patterns)] // diplomat_core uses non_exhaustive a lot; we should never miss its patterns
+
 pub mod c;
 pub mod c2;
 pub mod common;
