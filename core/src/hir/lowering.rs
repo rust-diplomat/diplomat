458f82a97a26c33a0deb4f08b72429bd5d9945da
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2023-11-01 20:14:25 +0100

    Update lowering.rs

--- core/src/hir/lowering.rs
+++ core/src/hir/lowering.rs
@@ -566,7 +566,7 @@ impl<'ast, 'errors> LoweringContext<'ast, 'errors> {
 
                             OutType::Opaque(OpaquePath::new(
                                 lifetimes,
-                                Optional(true),
+                                Optional(false),
                                 MaybeOwn::Own,
                                 tcx_id,
                             ))
