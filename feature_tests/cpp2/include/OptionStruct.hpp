a7fe6250b327b30f7a1ab32d725f8e9c19db76a8
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2023-11-01 14:31:52 -0700

    whoops (#351)

--- feature_tests/cpp2/include/OptionStruct.hpp
+++ feature_tests/cpp2/include/OptionStruct.hpp
@@ -18,10 +18,10 @@
 
 inline capi::OptionStruct OptionStruct::AsFFI() const {
   return capi::OptionStruct {
-    .a = a.AsFFI(),
-    .b = b.AsFFI(),
+    .a = a ? a->AsFFI() : nullptr,
+    .b = b ? b->AsFFI() : nullptr,
     .c = c,
-    .d = d.AsFFI(),
+    .d = d ? d->AsFFI() : nullptr,
   };
 }
 
