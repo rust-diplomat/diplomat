69c2222c46dcbc9cb3af36f94a86b6b605237999
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2024-01-05 17:44:14 +0100

    writeable

--- tool/templates/dart/writeable.dart
+++ tool/templates/dart/writeable.dart
@@ -1,24 +1,24 @@
 final class _Writeable {
   final ffi.Pointer<ffi.Opaque> _underlying;
 
-  _Writeable() : _underlying = _create(0);
-  static final _create =
-    _capi<ffi.NativeFunction<ffi.Pointer<ffi.Opaque> Function(ffi.Size)>>('diplomat_buffer_writeable_create')
-    .asFunction<ffi.Pointer<ffi.Opaque> Function(int)>();
-
+  _Writeable() : _underlying = _diplomat_buffer_writeable_create(0);
+  
   String finalize() {
-    final string = Utf8Decoder().convert(_getBytes(_underlying).asTypedList(_len(_underlying)));
-    _destroy(_underlying);
+    final string = Utf8Decoder().convert(_diplomat_buffer_writeable_get_bytes(_underlying).asTypedList(_diplomat_buffer_writeable_len(_underlying)));
+    _diplomat_buffer_writeable_destroy(_underlying);
     return string;
   }
-  static final _len = 
-    _capi<ffi.NativeFunction<ffi.Size Function(ffi.Pointer<ffi.Opaque>)>>('diplomat_buffer_writeable_len')
-    .asFunction<int Function(ffi.Pointer<ffi.Opaque>)>(isLeaf: true);
-
-  static final _getBytes = 
-    _capi<ffi.NativeFunction<ffi.Pointer<ffi.Uint8> Function(ffi.Pointer<ffi.Opaque>)>>('diplomat_buffer_writeable_get_bytes')
-    .asFunction<ffi.Pointer<ffi.Uint8> Function(ffi.Pointer<ffi.Opaque>)>(isLeaf: true);
-  static final _destroy =
-    _capi<ffi.NativeFunction<ffi.Void Function(ffi.Pointer<ffi.Opaque>)>>('diplomat_buffer_writeable_destroy')
-    .asFunction<void Function(ffi.Pointer<ffi.Opaque>)>(isLeaf: true);
 }
+
+  
+@ffi.Native<ffi.Pointer<ffi.Opaque> Function(ffi.Size)>(symbol: 'diplomat_buffer_writeable_create', isLeaf: true)
+external ffi.Pointer<ffi.Opaque> _diplomat_buffer_writeable_create(int len);
+
+@ffi.Native<ffi.Size Function(ffi.Pointer<ffi.Opaque>)>(symbol: 'diplomat_buffer_writeable_len', isLeaf: true)
+external int _diplomat_buffer_writeable_len(ffi.Pointer<ffi.Opaque> ptr);
+
+@ffi.Native<ffi.Pointer<ffi.Uint8> Function(ffi.Pointer<ffi.Opaque>)>(symbol: 'diplomat_buffer_writeable_get_bytes', isLeaf: true)
+external ffi.Pointer<ffi.Uint8> _diplomat_buffer_writeable_get_bytes(ffi.Pointer<ffi.Opaque> ptr);
+
+@ffi.Native<ffi.Void Function(ffi.Pointer<ffi.Opaque>)>(symbol: 'diplomat_buffer_writeable_destroy', isLeaf: true)
+external void _diplomat_buffer_writeable_destroy(ffi.Pointer<ffi.Opaque> ptr);
