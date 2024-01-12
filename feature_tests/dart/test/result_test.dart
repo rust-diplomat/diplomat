bc99db98529b7fd490f1887409d4c01f320a6646
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2024-01-12 15:08:43 +0100

    a

--- feature_tests/dart/test/result_test.dart
+++ feature_tests/dart/test/result_test.dart
@@ -9,6 +9,7 @@ void main() {
         expect(ResultOpaque.failingBar, throwsA(ErrorEnum.bar));
         expect(ResultOpaque.failingUnit, throwsA(VoidError()));
         expect(() => ResultOpaque.failingStruct(109), throwsA(ErrorStruct()..i = 109..j = 12));
+
         expect(() => ResultOpaque.newInErr(559), throwsA(ResultOpaqueMatcher(559)));
         expect(() => ResultOpaque.newInEnumErr(881), throwsA(ResultOpaqueMatcher(881)));
 
