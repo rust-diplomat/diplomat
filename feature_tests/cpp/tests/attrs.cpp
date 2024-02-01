d540ffeaed5f24dc71ebf5030418bef47f503a47
Manish Goregaokar <manishsmail@gmail.com>
2024-01-31 20:25:03 -0800

    Add attrs test

---
+++ feature_tests/cpp/tests/attrs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "../include/AttrOpaque1.hpp"
+#include "assert.hpp"
+
+int main(int argc, char *argv[]) {
+    AttrOpaque1 o = AttrOpaque1::new_();
+    // the cpp2 renames don't apply. However, these must link correctly!!
+    simple_assert_eq("method should call", o.method(), 77);
+    simple_assert_eq("method should call", o.crenamed(), 123);
+
+    // These C names should also resolve
+    void* renamed = (void*)capi::renamed_in_c_only;
+    std::cout<<"Renamed function at "<<renamed<<std::endl;
+    renamed = (void*)capi::namespace_AttrOpaque1_method;
+    std::cout<<"Renamed function at "<<renamed<<std::endl;
+}
