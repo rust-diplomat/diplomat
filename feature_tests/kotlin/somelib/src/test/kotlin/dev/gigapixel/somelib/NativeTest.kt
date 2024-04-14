72aa34214fcf875c6fb3f50fb5e06f252526a59c
Jan Cristina <jan.cristina@gmail.com>
2024-04-14 07:41:26 +0200

    jna-kotlin-slice-str - fix tests

--- feature_tests/kotlin/somelib/src/test/kotlin/dev/gigapixel/somelib/NativeTest.kt
+++ feature_tests/kotlin/somelib/src/test/kotlin/dev/gigapixel/somelib/NativeTest.kt
@@ -8,15 +8,13 @@ import kotlin.test.assertEquals
 
 class NativeTest {
     @Test
+    @ExperimentalUnsignedTypes
     fun testNative() {
-        val globalMemSesh = MemorySession.global()
-        val segAlloc = SegmentAllocator.newNativeArena(globalMemSesh)
         val random =  Random(2984751)
         for (run in 0 .. 100) {
             val array = random.nextBytes(1000)
-            val mem = PrimitiveArrayTools.native(array)
-            val ptr = mem.share(0)
-            val got = ptr.getByteArray(0, array.size)
+            val (mem, slice) = PrimitiveArrayTools.native(array)
+            val got = slice.data.getByteArray(0, array.size)
             mem.close()
             assertEquals(got.toList(), array.toList())
         }
@@ -24,9 +22,8 @@ class NativeTest {
         for (run in 0 .. 100) {
             val array = random.nextBytes(1000)
             val uByteArray = array.map { it.toUByte()}.toUByteArray()
-            val mem = PrimitiveArrayTools.native(uByteArray)
-            val ptr = mem.share(0)
-            val got = ptr.getByteArray(0, array.size)
+            val (mem, slice) = PrimitiveArrayTools.native(array)
+            val got = slice.data.getByteArray(0, array.size).asUByteArray()
             mem.close()
             assertEquals(got.map { it.toUByte()}, uByteArray.toList())
         }
@@ -35,9 +32,8 @@ class NativeTest {
         for (run in 0 .. 100) {
             val size = 1000
             val intArray = (0..size).map { random.nextInt() }.toIntArray()
-            val mem = PrimitiveArrayTools.native(intArray)
-            val ptr = mem.share(0)
-            val got = ptr.getIntArray(0, intArray.size)
+            val (mem, slice) = PrimitiveArrayTools.native(intArray)
+            val got = slice.data.getIntArray(0, intArray.size)
             mem.close()
             assertEquals(got.toList(), intArray.toList())
         }
@@ -45,9 +41,8 @@ class NativeTest {
         for (run in 0 .. 100) {
             val size = 1000
             val intArray = (0..size).map { random.nextInt().toUInt() }.toUIntArray()
-            val mem = PrimitiveArrayTools.native(intArray)
-            val ptr = mem.share(0)
-            val got = ptr.getIntArray(0, intArray.size).asUIntArray()
+            val (mem, slice) = PrimitiveArrayTools.native(intArray)
+            val got = slice.data.getIntArray(0, intArray.size).asUIntArray()
             mem.close()
             assertEquals(got.toList(), intArray.toList())
         }
