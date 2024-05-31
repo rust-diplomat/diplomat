1c98b047b0eed244df5ce8cb1b45b0a64689c949
Tyler K <ambiguousemails@proton.me>
2024-05-31 15:23:51 -0700

    .d.ts import syntax fix

--- feature_tests/js2/api/UnimportedEnum.d.ts
+++ feature_tests/js2/api/UnimportedEnum.d.ts
@@ -1,4 +1,4 @@
-import { u8, i8, u16, i16, u32, i32, u64, i64, usize, isize, f32, f64, pointer, char } from "./diplomat-runtime.d.ts";
+import type { u8, i8, u16, i16, u32, i32, u64, i64, usize, isize, f32, f64, pointer, char } from "./diplomat-runtime.d.ts";
 
 // Base enumerator definition
 export class UnimportedEnum {
