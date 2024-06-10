b5f5bbdb94ddd4064985184959972f6ba9a4f319
Tyler K <ambiguousemails@proton.me>
2024-06-10 14:43:08 -0700

    Rewriting struct test

--- feature_tests/js2/test/struct.mjs
+++ feature_tests/js2/test/struct.mjs
@@ -2,12 +2,12 @@ import test from 'ava';
 import { MyEnum, MyStruct } from "diplomat-wasm-js2-feature-tests";
 
 test("Verify invariants of struct", t => {
-    const s = MyStruct.new("hello");
-    t.is(s["a"], 17);
-    t.is(s["b"], true);
-    t.is(s["c"], 209);
-    t.is(s["d"], 1234n);
-    t.is(s["e"], 5991);
-    t.is(s["f"], "餐");
-    t.is(s["g"], MyEnum.B);
+    const s = MyStruct.new_("hello");
+    t.is(s.a, 17);
+    t.is(s.b, true);
+    t.is(s.c, 209);
+    t.is(s.d, 1234n);
+    t.is(s.e, 5991);
+    t.is(s.f, "餐");
+    t.is(s.g, MyEnum.B);
 });
