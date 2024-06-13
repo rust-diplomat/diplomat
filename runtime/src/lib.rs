56526c4f797d92da747273f0e94ae0ec56d0d787
Jan Cristina <jan.cristina@gmail.com>
2024-06-13 22:24:43 +0200

    kotlin-benchmark - basic benchmark

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -10,6 +10,7 @@ mod wasm_glue;
 
 mod write;
 pub use write::DiplomatWrite;
+pub use write::{diplomat_buffer_write_create, diplomat_buffer_write_destroy};
 
 mod result;
 pub use result::DiplomatResult;
