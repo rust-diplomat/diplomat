7e86d0c3e82d0232c72feb6d42f535e5265f7e61
Tyler K <tyly04@gmail.com>
2024-06-17 10:22:29 -0700

    Rewriting test to match args

--- example/js2/test/fixed-decimal.mjs
+++ example/js2/test/fixed-decimal.mjs
@@ -3,9 +3,9 @@ import test from "ava";
 import { ICU4XFixedDecimalFormatterDemo } from "demo/demo"; 
 
 test("Format write exists.", t => {
-	t.notThrows(() => {ICU4XFixedDecimalFormatterDemo.formatWrite()});
+	t.notThrows(() => {ICU4XFixedDecimalFormatterDemo.formatWrite("en", 10)});
 });
 
 test("Format write returns a formatted decimal.", t => {
-	t.is("1,000", ICU4XFixedDecimalFormatterDemo.formatWrite(1000));
+	t.is("1,000", ICU4XFixedDecimalFormatterDemo.formatWrite("en", 1000));
 });
