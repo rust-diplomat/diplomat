c00e6e3f0f14581971ff7537f17594077b449dfc
Tyler K <tyly04@gmail.com>
2024-06-17 10:57:00 -0700

    Setting up cleaner indentation

--- example/demo-gen/demo/ICU4XFixedDecimalFormatter.mjs
+++ example/demo-gen/demo/ICU4XFixedDecimalFormatter.mjs
@@ -5,37 +5,22 @@ import { ICU4XLocale } from "../ICU4XLocale.mjs"
 
 export function formatWrite(name, v) {
 	return ((...args) => {
-    return this.formatWrite(args[1], args[2]);
-})
-.call(
-    
-    ICU4XFixedDecimalFormatter.tryNew.call(
-    
-    null,
-    
-    ICU4XLocale.new_.call(
-    
-    null,
-    
-    arguments[0]
-    
-),
-    
-    ICU4XDataProvider.newStatic.call(
-    
-    null
-    
-)
-    
-),
-    
-    ICU4XFixedDecimal.new_.call(
-    
-    null,
-    
-    arguments[1]
-    
-)
-    
-);
+        return this.formatWrite(args[1], args[2]);
+    })
+    .call(
+        ICU4XFixedDecimalFormatter.tryNew.call(
+            null,
+            ICU4XLocale.new_.call(
+                null,
+                arguments[0]
+            ),
+            ICU4XDataProvider.newStatic.call(
+                null
+            )
+        ),
+        ICU4XFixedDecimal.new_.call(
+            null,
+            arguments[1]
+        )
+    );
 }
