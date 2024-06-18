d147b05ee070667a9cb08de74bdf03e0290cc2a7
Tyler K <tyly04@gmail.com>
2024-06-17 19:05:16 -0700

    Merging test into just one function

--- example/js2/test/fixed-decimal.mjs
+++ example/js2/test/fixed-decimal.mjs
@@ -1,11 +1,8 @@
 import test from "ava";
 
 import { ICU4XFixedDecimalFormatterDemo } from "demo/demo"; 
-
-test("Format write exists.", t => {
-	t.notThrows(() => {ICU4XFixedDecimalFormatterDemo.formatWrite("en", 10)});
-});
+import { ICU4XFixedDecimalGroupingStrategy } from "../api/ICU4XFixedDecimalGroupingStrategy.mjs";
 
 test("Format write returns a formatted decimal.", t => {
-	t.is("1,000", ICU4XFixedDecimalFormatterDemo.formatWrite("en", 1000));
+	t.is("1,000", ICU4XFixedDecimalFormatterDemo.formatWrite("en", ICU4XFixedDecimalGroupingStrategy.Auto, false, 1000));
 });
