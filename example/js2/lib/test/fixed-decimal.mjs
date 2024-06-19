b5807401381f93aa158e7c9855bb465618121961
Tyler K <tyly04@gmail.com>
2024-06-19 14:46:14 -0700

    Adding new test for toString()

--- example/js2/lib/test/fixed-decimal.mjs
+++ example/js2/lib/test/fixed-decimal.mjs
@@ -1,8 +1,12 @@
 import test from "ava";
 
-import { ICU4XFixedDecimalFormatterDemo } from "demo/demo"; 
+import { ICU4XFixedDecimalDemo, ICU4XFixedDecimalFormatterDemo } from "demo/demo"; 
 import { ICU4XFixedDecimalGroupingStrategy } from "../api/ICU4XFixedDecimalGroupingStrategy.mjs";
 
 test("Format write returns a formatted decimal.", t => {
 	t.is("1,000", ICU4XFixedDecimalFormatterDemo.formatWrite("en", ICU4XFixedDecimalGroupingStrategy.Auto, false, 1000));
+});
+
+test("To string returns the proper string.", t => {
+	t.is(1000, ICU4XFixedDecimalDemo.toString(1000));
 });
