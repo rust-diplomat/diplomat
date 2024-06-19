3c3007d52b895c7c3155e93190e262dc576507fb
Tyler K <tyly04@gmail.com>
2024-06-18 17:59:12 -0700

    Started on parameters

--- example/js2/app/src/index.js
+++ example/js2/app/src/index.js
@@ -1,8 +1,28 @@
 import { RenderInfo } from "mini-icu4x/demo";
 
+class EnumTemplate extends HTMLElement {
+	static template = document.querySelector("template#enum").content;
+	constructor(choices) {
+		super();
+
+		const shadowRoot = this.attachShadow({ mode: "open" });
+		shadowRoot.appendChild(EnumTemplate.template.cloneNode(true));
+	}
+}
+
+customElements.define("terminus-param-enum", EnumTemplate);
+
 class TerminusParams extends HTMLElement {
 	constructor(params){
 		super();
+		for (let param of params) {
+			switch (param.type) {
+				// Assume it's an enum if we don't recognize the type.
+				default:
+					this.appendChild(new EnumTemplate());
+					break;
+			}
+		}
 	}
 }
 
@@ -30,5 +50,5 @@ class TerminusRender extends HTMLElement {
 customElements.define("terminus-render", TerminusRender);
 
 RenderInfo.termini.forEach((t) => {
-	document.body.appendChild(new TerminusRender(t.func.name));
+	document.body.appendChild(new TerminusRender(t.func.name, t.parameters));
 });
