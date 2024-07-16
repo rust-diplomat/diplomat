f63dcd967e581ecaf8268a93de662ec2935d2df3
Ellen Arteca <emarteca@google.com>
2024-07-15 17:22:53 -0700

    Manual example of callback into Kotlin (primitive arg, no return

---
+++ example/simple_testing/kotlin/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+    `maven-publish`
+}
+
+group = "dev.diplomattest"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// declare a "configuration" named "someConfiguration"
+val someConfiguration by configurations.creating
+
+dependencies {
+    implementation("net.java.dev.jna:jna:5.14.0")
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.diplomattest"
+            artifactId = "simple_testing"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
+}
+
+
+
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(19)
+}
+
+application {
+    mainClass = "dev.diplomattest.simple_testing.Main"
+}
+
+run {
+}
