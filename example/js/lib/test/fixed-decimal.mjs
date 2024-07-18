733d4e2ef316db2c828a51f8b984809ad26ad93a
Tyler K <ambiguousemails@proton.me>
2024-07-18 12:49:11 -0700

    Patching example js lib tests

--- example/js/lib/test/fixed-decimal.mjs
+++ example/js/lib/test/fixed-decimal.mjs
@@ -1,12 +1,12 @@
 import test from "ava";
 
-import { ICU4XFixedDecimalDemo, ICU4XFixedDecimalFormatterDemo } from "demo/demo"; 
-import { ICU4XFixedDecimalGroupingStrategy } from "../api/ICU4XFixedDecimalGroupingStrategy.mjs";
+import { FixedDecimalDemo, FixedDecimalFormatterDemo } from "demo/demo"; 
+import { FixedDecimalGroupingStrategy } from "demo";
 
 test("Format write returns a formatted decimal.", t => {
-	t.is("1,000", ICU4XFixedDecimalFormatterDemo.formatWrite("en", ICU4XFixedDecimalGroupingStrategy.Auto, false, 1000));
+	t.is("1,000", FixedDecimalFormatterDemo.formatWrite("en", FixedDecimalGroupingStrategy.Auto, false, 1000));
 });
 
 test("toString returns the proper string.", t => {
-	t.is("1000", ICU4XFixedDecimalDemo.toString(1000));
+	t.is("1000", FixedDecimalDemo.toString(1000));
 });
