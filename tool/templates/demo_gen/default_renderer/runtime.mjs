0ddd3dcc4b6b6e3182f90062a1431e86c03c931b
Tyler K <ambiguousemails@proton.me>
2024-07-19 15:51:29 -0700

    Converting to spaces

--- tool/templates/demo_gen/default_renderer/runtime.mjs
+++ tool/templates/demo_gen/default_renderer/runtime.mjs
@@ -8,31 +8,31 @@ var currentlySelected = null;
 var navSelected = null;
 
 RenderInfo.termini.forEach((t) => {
-	var renderOut = new TerminusRender(t, lib, null);
-	document.getElementById("termini").appendChild(renderOut);
-	
-	renderOut.classList.add("tab-pane");
-	renderOut.role = "tabpanel";
-	
-	var outNav = document.createElement("li");
-	outNav.innerHTML = `<button class="nav-link ${navSelected === null ? "active" : ""}" id="${t.funcName}-nav" data-toggle="pill" role="tab" aria-selected="${navSelected === null}" aria-controls="${t.funcName}">${t.funcName}</button>`;
-	outNav.classList.add("nav-item");
-	outNav.addEventListener("click", () => {
-		currentlySelected.classList.remove("active");
-		navSelected.classList.remove("active");
+    var renderOut = new TerminusRender(t, lib, null);
+    document.getElementById("termini").appendChild(renderOut);
+    
+    renderOut.classList.add("tab-pane");
+    renderOut.role = "tabpanel";
+    
+    var outNav = document.createElement("li");
+    outNav.innerHTML = `<button class="nav-link ${navSelected === null ? "active" : ""}" id="${t.funcName}-nav" data-toggle="pill" role="tab" aria-selected="${navSelected === null}" aria-controls="${t.funcName}">${t.funcName}</button>`;
+    outNav.classList.add("nav-item");
+    outNav.addEventListener("click", () => {
+        currentlySelected.classList.remove("active");
+        navSelected.classList.remove("active");
 
-		renderOut.classList.add("active");
-		outNav.children[0].classList.add("active");
+        renderOut.classList.add("active");
+        outNav.children[0].classList.add("active");
 
-		navSelected = outNav.children[0];
-		currentlySelected = renderOut;
-	});
+        navSelected = outNav.children[0];
+        currentlySelected = renderOut;
+    });
 
-	nav.appendChild(outNav);
-	
-	if (currentlySelected === null) {
-		renderOut.classList.add("active");
-		currentlySelected = renderOut;
-		navSelected = outNav.children[0];
-	}
+    nav.appendChild(outNav);
+    
+    if (currentlySelected === null) {
+        renderOut.classList.add("active");
+        currentlySelected = renderOut;
+        navSelected = outNav.children[0];
+    }
 });
