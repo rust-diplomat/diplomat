410af0f8516a0d7b47b694e9c9b1579a12d2c4e1
Tyler K <tyly04@gmail.com>
2024-07-19 18:45:11 -0700

    Updating tests

---
+++ feature_tests/js/test/attrs-ts.mjs
@@ -0,0 +1,10 @@
+import test from "ava";
+import { MyIterable } from "diplomat-wasm-js2-feature-tests";
+test("Verify Iterables and Iterators", t => {
+    let iterable = MyIterable.new_([10, 20, 30, 40, 50]);
+    let start = 10;
+    for (let i of iterable) {
+        t.is(i, start);
+        start += 10;
+    }
+});
