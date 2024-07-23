a51bc38f198247f1b8d00bbd380eb2386de3ffe7
Manish Goregaokar <manishsmail@gmail.com>
2024-07-23 04:04:49 -0700

    comparators

--- feature_tests/src/attrs.rs
+++ feature_tests/src/attrs.rs
@@ -61,7 +61,7 @@ pub mod ffi {
     }
 
     #[diplomat::opaque]
-    #[diplomat::attr(any(c, kotlin, cpp, js), disable)]
+    #[diplomat::attr(not(supports = comparators), disable)]
     pub struct Comparable(u8);
 
     impl Comparable {
