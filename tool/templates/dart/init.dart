d4c5146d3a8c26d8884bfd3d4a9e5d653be72d6f
Robert Bastian <robertbastian@unicode.org>
2024-07-23 15:49:32 +0200

    docs

--- tool/templates/dart/init.dart
+++ tool/templates/dart/init.dart
@@ -1,16 +1,15 @@
 /// A [Rune] is a Unicode code point, such as `a`, or `💡`.
 /// 
 /// The recommended way to obtain a [Rune] is to create it from a 
-/// [String], which is conceptually a list of [Runes]. For example,
-/// `'a'.runes.first` is equal to the [Rune] `a`.
+/// [String], which is conceptually a sequence of [Rune]s. For
+/// example, `'a'.runes.first` is equal to the [Rune] `a`.
 /// 
-/// Dart does not have a character/rune literal, so integer literals
-/// need to be used. For example the Unicode code point U+1F4A1, `💡`,
-/// can be represented by `0x1F4A1`. Note that only values in the ranges
-/// `0x0..0xD7FF` and `0xE000..0x10FFFF` (both inclusive) are Unicode
-/// code points, and hence valid [Rune]s.
+/// Dart does not have a character/rune literal (https://github.com/dart-lang/language/issues/886),
+/// so integer literals need to be used. For example the Unicode code point 
+/// U+1F4A1, `💡`, can be represented by `0x1F4A1`.
 ///
-/// A [String] can be constructed from a [Rune] using [String.fromCharCode]. 
+/// A [String] can be constructed from a [Rune] using (the [confusingly named](
+/// https://github.com/dart-lang/sdk/issues/56304)) [String.fromCharCode]. 
 typedef Rune = int;
 
 // ignore: unused_element
