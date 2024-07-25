b98183d52bf4706c35dbb56dd836239d966ff4c1
Tyler K <tyly04@gmail.com>
2024-07-25 13:46:03 -0700

    Adding barebones navigation

--- example/demo_gen/demo/index.mjs
+++ example/demo_gen/demo/index.mjs
@@ -10,6 +10,7 @@ export const RenderInfo = {
             func: FixedDecimalFormatterDemo.formatWrite,
             // For avoiding webpacking minifying issues:
             funcName: "FixedDecimalFormatter.formatWrite",
+            html: "rendering/FixedDecimalFormatter_formatWrite.html",
             parameters: [
                 
                 {
@@ -39,6 +40,7 @@ export const RenderInfo = {
             func: FixedDecimalDemo.toString,
             // For avoiding webpacking minifying issues:
             funcName: "FixedDecimal.toString",
+            html: "rendering/FixedDecimal_toString.html",
             parameters: [
                 
                 {
