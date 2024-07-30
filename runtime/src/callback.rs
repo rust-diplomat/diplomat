51bee71abf0b9c8fc3b54728f06a67a7a1bc9727
Ellen Arteca <emarteca@google.com>
2024-07-30 16:07:03 -0700

    generating a wrapper DiplomatCallback struct for each return type; no longer only working for languages with function pointers

--- runtime/src/callback.rs
+++ runtime/src/callback.rs
@@ -8,13 +8,17 @@ pub struct DiplomatCallback<ReturnType> {
     // callback wrapper object in the foreign runtime + the runtime itself
     pub data: *const c_void,
     // function to actually run the callback
-    run_callback: unsafe extern "C" fn(*const c_void, ...) -> ReturnType, 
+    pub run_callback: unsafe extern "C" fn(*const c_void, ...) -> ReturnType,
     // function to destroy this callback struct
-    destructor: unsafe extern "C" fn(*const c_void),
+    destructor: Option<unsafe extern "C" fn(*const c_void)>,
 }
 
 impl<ReturnType> Drop for DiplomatCallback<ReturnType> {
     fn drop(&mut self) {
-        unsafe { (self.destructor)(self.data); }
+        if let Some(destructor) = self.destructor {
+            unsafe {
+                (destructor)(self.data);
+            }
+        }
     }
 }
