4063f30950489ef29994d7c46046fb8021cac912
Tyler K <tyly04@gmail.com>
2024-08-05 14:23:21 -0700

    Console.error instead of throwing

--- tool/templates/demo_gen/default_renderer/runtime.mjs
+++ tool/templates/demo_gen/default_renderer/runtime.mjs
@@ -6,7 +6,7 @@ let params = new URLSearchParams(window.location.search);
 let func = params.get("func");
 
 let terminus = new TerminusRender(lib, (param, updateParamEvent) => {
-    throw Error(`Unrecognized parameter type ${param}`);
+    console.error(`Unrecognized parameter type ${param}`);
 }, RenderInfo.termini[func]);
 
 document.getElementById("render").appendChild(terminus);
