c1b07b392d08d505041d9a1f45e0cc20feb0b0a9
Tyler K <tyly04@gmail.com>
2024-08-05 14:27:51 -0700

    Updated generation

--- example/demo_gen/demo/rendering/runtime.mjs
+++ example/demo_gen/demo/rendering/runtime.mjs
@@ -5,6 +5,8 @@ let params = new URLSearchParams(window.location.search);
 
 let func = params.get("func");
 
-let terminus = new TerminusRender(lib, () => {}, RenderInfo.termini[func]);
+let terminus = new TerminusRender(lib, (param, updateParamEvent) => {
+    console.error(`Unrecognized parameter type ${param}`);
+}, RenderInfo.termini[func]);
 
 document.getElementById("render").appendChild(terminus);
