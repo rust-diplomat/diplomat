5b252befcc9c7aa1c046e5509053a15cc316dca0
Ellen Arteca <emarteca@google.com>
2024-08-06 13:57:47 -0400

    updated kotlin example to match the current generated rust and c

--- example/simple_testing/kotlin/src/main/kotlin/dev/diplomattest/simple_testing/Main.kt
+++ example/simple_testing/kotlin/src/main/kotlin/dev/diplomattest/simple_testing/Main.kt
@@ -4,43 +4,64 @@ import com.sun.jna.Callback
 import com.sun.jna.Library
 import com.sun.jna.Native
 import com.sun.jna.Pointer
-
+import com.sun.jna.Structure
 
 interface DiplomatCallback_Lib: Library {
-    // general use
-    fun diplomat_callback_create_for_jvm__callback(callback_wrap: Callback): Pointer
-
     // specific to this callback type
-    interface DiplomatCallbackI32ToI32: Callback {
-        fun invoke(input: Int): Int
+    // this is the "run_callback" callback
+    interface Runner_DiplomatCallback_Wrapper_test_rust_fn_callback_f: Callback {
+        fun invoke(ignored: Pointer?, input: Int): Int
     }
     // specific to this callback
-    fun DiplomatCallbackI32ToI32_test_rust_fn_test_call(diplomatCallback: Pointer): Int
+    fun Wrapper_test_multi_arg_callback(diplomatCallback: DiplomatCallback_Wrapper_test_rust_fn_callback_f_Native, i: Int): Int
 }
 
-class DiplomatCallbackI32ToI32(
-    val callback_pointer: Pointer,
-    val callback: DiplomatCallback_Lib.DiplomatCallbackI32ToI32,
-)  {
+class DiplomatCallback_Wrapper_test_rust_fn_callback_f_Native: Structure(), Structure.ByValue {
+    @JvmField
+    internal var dataa: Pointer = Pointer(0L);
+    @JvmField
+    internal var run_callback: DiplomatCallback_Lib.Runner_DiplomatCallback_Wrapper_test_rust_fn_callback_f
+        = object :  DiplomatCallback_Lib.Runner_DiplomatCallback_Wrapper_test_rust_fn_callback_f {
+                override fun invoke(ignored: Pointer?, input: Int): Int {
+                    throw Exception("ERROR NOT IMPLEMENTED")
+                }
+            }
+    @JvmField
+    internal var destructor: Pointer = Pointer(0L);
+  
+    // Define the fields of the struct
+    override fun getFieldOrder(): List<String> {
+        return listOf("dataa", "run_callback", "destructor")
+    }
+}
+
+class DiplomatCallback_Wrapper_test_rust_fn_callback_f internal constructor (
+    internal val nativeStruct: DiplomatCallback_Wrapper_test_rust_fn_callback_f_Native) {
+    val dataa: Pointer = nativeStruct.dataa
+    val run_callback: Callback = nativeStruct.run_callback
+    val destructor: Pointer = nativeStruct.destructor
 
     companion object {
+        val NATIVESIZE: Long = Native.getNativeSize(DiplomatCallback_Wrapper_test_rust_fn_callback_f_Native::class.java).toLong()
         internal val libClass: Class<DiplomatCallback_Lib> = DiplomatCallback_Lib::class.java
         internal val lib: DiplomatCallback_Lib = Native.load("somelib", libClass)
 
-        fun test_rust_fn_call_with_callback(dc: DiplomatCallbackI32ToI32): Int {
-            return lib.DiplomatCallbackI32ToI32_test_rust_fn_test_call(dc.callback_pointer);
-        }
-
-        fun fromCallback(cb: (Int) -> Int): DiplomatCallbackI32ToI32 {
-            val callback: DiplomatCallback_Lib.DiplomatCallbackI32ToI32 = object :  DiplomatCallback_Lib.DiplomatCallbackI32ToI32 {
-                override fun invoke(input: Int): Int {
+        fun fromCallback(cb: (Int) -> Int): DiplomatCallback_Wrapper_test_rust_fn_callback_f {
+            val callback: DiplomatCallback_Lib.Runner_DiplomatCallback_Wrapper_test_rust_fn_callback_f = object :  DiplomatCallback_Lib.Runner_DiplomatCallback_Wrapper_test_rust_fn_callback_f {
+                override fun invoke(ignored: Pointer?, input: Int): Int {
                     return cb(input);
                 }
             }
-            val callback_pointer = lib.diplomat_callback_create_for_jvm__callback(callback);
-            return DiplomatCallbackI32ToI32(callback_pointer, callback)
+            val cb_wrap = DiplomatCallback_Wrapper_test_rust_fn_callback_f_Native()
+            cb_wrap.run_callback = callback;
+            return DiplomatCallback_Wrapper_test_rust_fn_callback_f(cb_wrap)
+        }
+
+        fun call_test_rust_fn(cb_wrap: DiplomatCallback_Wrapper_test_rust_fn_callback_f, i: Int): Int {
+            return lib.Wrapper_test_multi_arg_callback(cb_wrap.nativeStruct, i);
         }
     }
+
 }
 
 object Main {
@@ -51,8 +72,8 @@ object Main {
     
     @JvmStatic
     fun main(args: Array<String>) {
-        var cb_wrap = DiplomatCallbackI32ToI32.fromCallback(::callback)
-        var res = DiplomatCallbackI32ToI32.test_rust_fn_call_with_callback(cb_wrap); // calls test_rust_fn with the callback
+        var cb_wrap = DiplomatCallback_Wrapper_test_rust_fn_callback_f.fromCallback(::callback)
+        var res = DiplomatCallback_Wrapper_test_rust_fn_callback_f.call_test_rust_fn(cb_wrap, 5); // calls test_rust_fn with the callback
         println("Result: " + res);
     }
 }
