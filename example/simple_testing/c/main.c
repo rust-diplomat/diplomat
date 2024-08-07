c5b7ca58080d64b78aa8fb0b8cb8dce1dd65c876
Ellen Arteca <emarteca@google.com>
2024-08-07 16:18:49 -0400

    revised example, getting rid of function pointer table in C

--- example/simple_testing/c/main.c
+++ example/simple_testing/c/main.c
@@ -5,42 +5,21 @@
 
 // -------------------------------------------------------------------------- generated code
 
-typedef struct TesterTraitFunctionPointers {
-    int32_t (*run_test_trait_fn_callback)(int32_t);
-    void (*run_test_void_trait_fn_callback)();
-} TesterTraitFunctionPointers;
-
 typedef struct TesterTrait_VTable {
-    int32_t (*run_test_trait_fn_callback)(TesterTraitFunctionPointers*, int32_t);
-    void (*run_test_void_trait_fn_callback)(TesterTraitFunctionPointers*);
+    int32_t (*run_test_trait_fn_callback)(void*, int32_t);
+    void (*run_test_void_trait_fn_callback)(void*);
     void (*destructor)(const void*);
 } TesterTrait_VTable;
 
 typedef struct DiplomatTraitStruct_TesterTrait {
-    TesterTraitFunctionPointers* data;
+    const void* data;
     TesterTrait_VTable* vtable;
 } DiplomatTraitStruct_TesterTrait;
 
 
-int32_t run_create_DiplomatTraitStruct_TesterTrait_test_trait_fn(TesterTraitFunctionPointers* cb_ptrs, int32_t arg0) {
-    return cb_ptrs->run_test_trait_fn_callback(arg0);
-}
-
-void run_create_DiplomatTraitStruct_TesterTrait_test_void_trait_fn(TesterTraitFunctionPointers* cb_ptrs) {
-    cb_ptrs->run_test_void_trait_fn_callback();
-}
-
 static void general_destructor(const void* data) {
     // TODO
 }
-
-
-TesterTrait_VTable tester_trait_vtable = {
-    run_create_DiplomatTraitStruct_TesterTrait_test_trait_fn,
-    run_create_DiplomatTraitStruct_TesterTrait_test_void_trait_fn,
-    general_destructor,
-};
-
 
 int32_t Wrapper_test_with_trait(DiplomatTraitStruct_TesterTrait tt, int32_t x);
 
@@ -53,16 +32,24 @@ void void_callback() {
     printf("CALLING CALLING CALLING CALLING from void callback\n");
 }
 
+int32_t run_create_DiplomatTraitStruct_TesterTrait_test_trait_fn(void* unused, int32_t arg0) {
+    return callback(arg0);
+}
+
+void run_create_DiplomatTraitStruct_TesterTrait_test_void_trait_fn(void* unused) {
+    void_callback();
+}
+
+TesterTrait_VTable tester_trait_vtable = {
+    run_create_DiplomatTraitStruct_TesterTrait_test_trait_fn,
+    run_create_DiplomatTraitStruct_TesterTrait_test_void_trait_fn,
+    general_destructor,
+};
+
 // -------------------------------------------------------------------------------------- main
 int main() {
-    TesterTraitFunctionPointers fct_ptrs = {
-        callback,
-        void_callback,
-    };
-
     DiplomatTraitStruct_TesterTrait tester_trait_struct = {
-        &fct_ptrs,
-        &tester_trait_vtable,
+        .vtable = &tester_trait_vtable,
     };
     int32_t res = Wrapper_test_with_trait(tester_trait_struct, 5);
     printf("omg is it working: %d\n", res); 
