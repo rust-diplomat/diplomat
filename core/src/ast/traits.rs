21e71fc070722cb3566406b078500e77fd85f77f
Ellen Arteca <emarteca@google.com>
2024-08-12 12:38:30 -0400

    partway through adding traits to the HIR

--- core/src/ast/traits.rs
+++ core/src/ast/traits.rs
@@ -10,9 +10,30 @@ pub struct Trait {
     pub name: Ident,
     pub docs: Docs,
     pub lifetimes: LifetimeEnv,
-    // pub fcts: Callback,
+    pub fcts: Vec<TraitFct>,
     pub attrs: Attrs,
 }
+
+#[derive(Clone, PartialEq, Eq, Hash, Serialize, Debug)]
+#[non_exhaustive]
+pub struct TraitFct {
+    pub name: Ident,
+    pub docs: Docs,
+    /// The `self` param of the function, if any.
+    pub self_param: Option<SelfParam>,
+    /// All non-`self` params taken by the function.
+    pub params: Vec<Param>,
+    /// The return type of the function, if any.
+    pub return_type: Option<TypeName>,
+    /// The lifetimes introduced in this function and surrounding impl block.
+    pub lifetime_env: LifetimeEnv,
+    /// The list of `cfg` attributes (if any).
+    ///
+    /// These are strings instead of `syn::Attribute` or `proc_macro2::TokenStream`
+    /// because those types are not `PartialEq`, `Hash`, `Serialize`, etc.
+    pub attrs: Attrs,
+}
+
 
 impl Trait {
 }
