f2084f6164023b11c1f253f8b90845064630c271
Tyler K <ambiguousemails@proton.me>
2024-08-16 11:24:11 -0700

    Adding Symbol construction with struct types
    Opaques next!

--- tool/src/js/type_generation/mod.rs
+++ tool/src/js/type_generation/mod.rs
@@ -232,7 +232,7 @@ impl<'ctx, 'tcx> TyGenContext<'ctx, 'tcx> {
 
             typescript: bool,
             mutable: bool,
-            has_default_constructor: bool,
+            is_out: bool,
 
             lifetimes: &'a LifetimeEnv,
             fields: &'a Vec<FieldInfo<'a, P>>,
@@ -245,7 +245,7 @@ impl<'ctx, 'tcx> TyGenContext<'ctx, 'tcx> {
             type_name,
 
             typescript,
-            has_default_constructor: is_out,
+            is_out,
             mutable: !is_out,
 
             lifetimes: &struct_def.lifetimes,
