8adbc7de3d40e0b4ced8a1276c2b35b9485d0bbe
Tyler K <tyly04@gmail.com>
2024-08-20 10:09:10 -0700

    Make char into string because there is no char type

--- tool/src/js/formatter.rs
+++ tool/src/js/formatter.rs
@@ -191,7 +191,8 @@ impl<'tcx> JSFormatter<'tcx> {
     pub fn fmt_primitive_list_type(&self, primitive: hir::PrimitiveType) -> &'static str {
         match primitive {
             hir::PrimitiveType::Bool => "Array<boolean>",
-            hir::PrimitiveType::Char => "Array<char>",
+            // TODO: Can't exactly test for characters with Typescript, but we could create a type guard?
+            hir::PrimitiveType::Char => "Array<string>",
             hir::PrimitiveType::Byte => "Uint8Array",
             hir::PrimitiveType::Int(hir::IntType::I64 | hir::IntType::U64) => "Array<bigint>",
             hir::PrimitiveType::Int(_)
