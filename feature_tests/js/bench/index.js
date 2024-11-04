5b47e75b05da48c0e6e32531bb090cd99ec5c3bc
Jan Cristina <jan.cristina@gmail.com>
2024-11-04 21:41:24 +0100

    remove bench code

--- feature_tests/js/bench/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var bench_harness_1 = require("@stdlib/bench-harness");
-var Opaque_1 = require("../api/Opaque");
-(0, bench_harness_1.default)("Init opaque", function () {
-    Opaque_1.Opaque.new_();
-});
--- feature_tests/js/bench/index.ts
+++
@@ -1,6 +0,0 @@
-import bench from "@stdlib/bench-harness";
-import { Opaque } from "../api/Opaque";
-
-bench("Init opaque", () => {
-  Opaque.new_();
-});
