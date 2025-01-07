762195f1cb44bc5da1426a2cf9a53f4fa26454f7
Tyler K <ambiguousemails@proton.me>
2025-01-07 13:27:41 -0800

    Full generation

---
+++ feature_tests/c/include/DefaultEnum.d.h
@@ -0,0 +1,23 @@
+#ifndef DefaultEnum_D_H
+#define DefaultEnum_D_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "diplomat_runtime.h"
+
+
+
+
+
+typedef enum DefaultEnum {
+  DefaultEnum_A = 0,
+  DefaultEnum_B = 1,
+} DefaultEnum;
+
+typedef struct DefaultEnum_option {union { DefaultEnum ok; }; bool is_ok; } DefaultEnum_option;
+
+
+
+#endif // DefaultEnum_D_H
