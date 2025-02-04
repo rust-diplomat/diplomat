265904862d2a74e8737da923fe3305f8bdbd5d02
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-02-04 14:02:24 -0800

    Adding test config files to feature_tests and example

--- tool/src/config.rs
+++ tool/src/config.rs
@@ -5,6 +5,8 @@ use crate::{demo_gen::DemoConfig, kotlin::KotlinConfig};
 #[derive(Clone, Default, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Config {
+	#[serde(rename="kotlin")]
 	pub kotlin_config : Option<KotlinConfig>,
+	#[serde(rename="demo-gen")]
 	pub demo_gen_config : Option<DemoConfig>
 }
