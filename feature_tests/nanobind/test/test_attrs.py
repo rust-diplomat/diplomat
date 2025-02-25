7485a6d04acdfae7ed4835a8f9962b83e40afeab
Walter Gray <walter@0m.dev>
2025-02-25 14:22:52 -0800

    Add basic test

--- feature_tests/nanobind/test/test_attrs.py
+++ feature_tests/nanobind/test/test_attrs.py
@@ -1,7 +1,7 @@
 import somelib
 from pprint import pp
 def test_attrs():
-    pass
-    #r = somelib.ns.AttrOpaque1Renamed.totally_not_new()
+    dir(somelib)
+    r = somelib.ns.AttrOpaque1Renamed.totally_not_new()
 
-    #assert r.method_renamed() == 77, "Method should call"
+    assert r.method_renamed() == 77, "Method should call"
