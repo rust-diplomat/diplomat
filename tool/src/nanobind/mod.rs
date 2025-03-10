c35c5d8a5d0cf301c2ae7142efbf63c06c883607
Walter Gray <walter@0m.dev>
2025-03-10 15:23:49 -0700

    Fix config integration

--- tool/src/nanobind/mod.rs
+++ tool/src/nanobind/mod.rs
@@ -2,9 +2,9 @@ mod binding;
 mod formatter;
 mod ty;
 
-use std::{borrow::Cow, collections::HashSet, path::Path};
+use std::{borrow::Cow, collections::HashSet};
 
-use crate::{ErrorStore, FileMap};
+use crate::{Config, ErrorStore, FileMap};
 use binding::Binding;
 use diplomat_core::hir::{self, BackendAttrSupport};
 use formatter::PyFormatter;
@@ -52,18 +52,16 @@ struct PythonConfig {
 
 pub(crate) fn run<'tcx>(
     tcx: &'tcx hir::TypeContext,
-    conf_path: Option<&Path>,
+    conf: Config,
 ) -> (FileMap, ErrorStore<'tcx, String>) {
-    let conf_path = conf_path.expect("Nanobind library needs to be called with config");
-
-    let conf_str = std::fs::read_to_string(conf_path)
-        .unwrap_or_else(|err| panic!("Failed to open config file {conf_path:?}: {err}"));
-    let PythonConfig { lib_name } = toml::from_str::<PythonConfig>(&conf_str)
-        .expect("Failed to parse config. Required field is 'lib_name'");
-
     let files = FileMap::default();
     let formatter = PyFormatter::new(tcx);
     let errors = ErrorStore::default();
+
+    let lib_name = conf
+        .shared_config
+        .lib_name
+        .expect("Nanobind backend requires lib_name to be set");
 
     let nanobind_filepath = format!("{lib_name}_ext.cpp");
     let mut binding = Binding::new();
