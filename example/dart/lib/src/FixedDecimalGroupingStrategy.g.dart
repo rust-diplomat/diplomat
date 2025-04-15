0d6b46341d2011f36ef87e81e55cda2c2b6513ad
Manish Goregaokar <manishsmail@gmail.com>
2025-04-15 10:46:30 -0700

    dart enum spaces

--- example/dart/lib/src/FixedDecimalGroupingStrategy.g.dart
+++ example/dart/lib/src/FixedDecimalGroupingStrategy.g.dart
@@ -5,16 +5,12 @@ part of 'lib.g.dart';
 
 enum FixedDecimalGroupingStrategy {
   /// Auto grouping
-  
   auto,
   /// No grouping
-  
   never,
   /// Always group
-  
   always,
   /// At least 2 groups
-  
   min2;
 
 }
