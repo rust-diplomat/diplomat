8fed0583124b3d41ff6157018aacfc7ad2ced391
Vaibhav Shukla <vaibhavshukla@google.com>
2025-05-23 20:46:51 +0000

    Resolve the merge conflicts of the local branch

--- example/kotlin/somelib/src/main/kotlin/dev/diplomattest/somelib/FixedDecimal.kt
+++ example/kotlin/somelib/src/main/kotlin/dev/diplomattest/somelib/FixedDecimal.kt
@@ -46,8 +46,8 @@ class FixedDecimal internal constructor (
             return returnOpaque
         }
     }
-    
-    /** Multiply the [FixedDecimal] by a given power of ten.
+    override
+    /** Multiply the [`FixedDecimal`] by a given power of ten.
     *
     *See the [Rust documentation for `multiply_pow10`](https://docs.rs/fixed_decimal/latest/fixed_decimal/struct.FixedDecimal.html#method.multiply_pow10) for more information.
     */
@@ -56,8 +56,8 @@ class FixedDecimal internal constructor (
         val returnVal = lib.icu4x_FixedDecimal_multiply_pow10_mv1(handle, power);
         
     }
-    
-    /** Format the [FixedDecimal] as a string.
+    override
+    /** Format the [`FixedDecimal`] as a string.
     *
     *See the [Rust documentation for `write_to`](https://docs.rs/fixed_decimal/latest/fixed_decimal/struct.FixedDecimal.html#method.write_to) for more information.
     */
