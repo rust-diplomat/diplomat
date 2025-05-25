1b748391e47c335166e2058d315556e7a371c2ec
Vaibhav Shukla <vaibhavshukla@google.com>
2025-05-25 15:25:13 +0000

    Fix the rust-fmt of kotlin/mod.rs

--- tool/src/kotlin/mod.rs
+++ tool/src/kotlin/mod.rs
@@ -1365,7 +1365,6 @@ returnVal.option() ?: return null
             .map(|method| self.gen_native_method_info(method, type_name))
             .collect::<Vec<_>>();
 
-
         let mut special_methods = SpecialMethods::default();
         let self_methods = ty
             .methods
