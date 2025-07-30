1d30b7c06de219b7e7837382223a354c2ac2c248
Tyler from Zeromatter <tyler@0m.dev>
2025-07-30 13:48:40 -0700

    Generation

--- feature_tests/c/include/CallbackWrapper.h
+++ feature_tests/c/include/CallbackWrapper.h
@@ -56,24 +56,24 @@ typedef struct DiplomatCallback_CallbackWrapper_test_slice_cb_arg_f {
     void (*run_callback)(const void*, DiplomatU8View );
     void (*destructor)(const void*);
 } DiplomatCallback_CallbackWrapper_test_slice_cb_arg_f;
+typedef struct DiplomatCallback_CallbackWrapper_test_result_output_t_result { bool is_ok;} DiplomatCallback_CallbackWrapper_test_result_output_t_result;
+
 typedef struct DiplomatCallback_CallbackWrapper_test_result_output_t {
     const void* data;
-    typedef struct DiplomatCallback_CallbackWrapper_test_result_output_t_result { bool is_ok;} DiplomatCallback_CallbackWrapper_test_result_output_t_result;
-DiplomatCallback_CallbackWrapper_test_result_output_t_result;
     DiplomatCallback_CallbackWrapper_test_result_output_t_result (*run_callback)(const void*);
     void (*destructor)(const void*);
 } DiplomatCallback_CallbackWrapper_test_result_output_t;
+typedef struct DiplomatCallback_CallbackWrapper_test_result_usize_output_t_result {union {size_t ok; }; bool is_ok;} DiplomatCallback_CallbackWrapper_test_result_usize_output_t_result;
+
 typedef struct DiplomatCallback_CallbackWrapper_test_result_usize_output_t {
     const void* data;
-    typedef struct DiplomatCallback_CallbackWrapper_test_result_usize_output_t_result {union {size_t ok; }; bool is_ok;} DiplomatCallback_CallbackWrapper_test_result_usize_output_t_result;
-DiplomatCallback_CallbackWrapper_test_result_usize_output_t_result;
     DiplomatCallback_CallbackWrapper_test_result_usize_output_t_result (*run_callback)(const void*);
     void (*destructor)(const void*);
 } DiplomatCallback_CallbackWrapper_test_result_usize_output_t;
+typedef struct DiplomatCallback_CallbackWrapper_test_option_output_t_result { bool is_ok;} DiplomatCallback_CallbackWrapper_test_option_output_t_result;
+
 typedef struct DiplomatCallback_CallbackWrapper_test_option_output_t {
     const void* data;
-    typedef struct DiplomatCallback_CallbackWrapper_test_option_output_t_result { bool is_ok;} DiplomatCallback_CallbackWrapper_test_option_output_t_result;
-DiplomatCallback_CallbackWrapper_test_option_output_t_result;
     DiplomatCallback_CallbackWrapper_test_option_output_t_result (*run_callback)(const void*);
     void (*destructor)(const void*);
 } DiplomatCallback_CallbackWrapper_test_option_output_t;
