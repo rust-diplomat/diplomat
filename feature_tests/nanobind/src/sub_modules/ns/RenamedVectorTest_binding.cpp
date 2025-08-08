17343937533958e2b7525bc507eb8d55f61aa423
Tyler from Zeromatter <tyler@0m.dev>
2025-08-08 15:19:25 -0700

    Generation

--- feature_tests/nanobind/src/sub_modules/ns/RenamedVectorTest_binding.cpp
+++ feature_tests/nanobind/src/sub_modules/ns/RenamedVectorTest_binding.cpp
@@ -13,7 +13,14 @@ void add_RenamedVectorTest_binding(nb::handle mod) {
         {0, nullptr}};
     
     nb::class_<ns::RenamedVectorTest>(mod, "RenamedVectorTest", nb::type_slots(ns_RenamedVectorTest_slots))
-    	.def("__getitem__", &ns::RenamedVectorTest::operator[], "idx"_a)
+    	.def("__getitem__", [](ns::RenamedVectorTest* self, size_t index) {
+    			auto out = self->operator[] (index);
+    			if (!out.has_value()) {
+    				throw nb::index_error("Could not get index.");
+    			} else {
+    				return out;
+    			}
+    		}, "idx"_a)
     	.def_prop_ro("len", &ns::RenamedVectorTest::len)
     	.def(nb::new_(&ns::RenamedVectorTest::new_))
     	.def("push", &ns::RenamedVectorTest::push, "value"_a);
