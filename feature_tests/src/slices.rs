11019f8969862fb9d25c724587cbc292f355bfa0
Tyler from Zeromatter <tyler@0m.dev>
2025-08-08 15:31:03 -0700

    Clippy

--- feature_tests/src/slices.rs
+++ feature_tests/src/slices.rs
@@ -141,7 +141,7 @@ pub mod ffi {
         #[diplomat::attr(auto, indexer)]
         pub fn get(&self, i: usize) -> Result<f64, ()> {
             if let Some(i) = self.0.get(i) {
-                Ok(i.clone())
+                Ok(*i)
             } else {
                 Err(())
             }
