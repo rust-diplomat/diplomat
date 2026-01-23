45a00ea5109bf7be3073ce155252f40df272d5fe
Manishearth <Manishearth@users.noreply.github.com>
2026-01-23 21:02:21 +0000

    deploy: 3ae143b6342d5ef3fb17a9f130939d71d69f3d00

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c320c467.js',
+                path_to_root + 'searchindex-1ae1f8d0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
