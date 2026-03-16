e14ed298d432c9d47cef1167514173eee2d15a19
Tyler from Zeromatter <tyler@0m.dev>
2026-03-16 16:27:35 -0700

    Use minijinja to process book

--- book/preprocessor/src/main.rs
+++ book/preprocessor/src/main.rs
@@ -5,9 +5,9 @@ use std::io;
 use askama::Template;
 use clap::{Parser, Subcommand};
 use diplomat_core::hir::BackendAttrSupport;
-use mdbook_preprocessor::book::{BookItem, Chapter};
 use mdbook_preprocessor::errors::Result;
 use mdbook_preprocessor::Preprocessor;
+use minijinja::{Environment, context};
 
 #[derive(Debug, Subcommand)]
 enum Supports {
@@ -37,27 +37,13 @@ fn main() {
 struct DiplomatPreprocessor;
 
 impl DiplomatPreprocessor {
-    fn find_languages_item(items: &mut Vec<BookItem>) -> Option<&mut Chapter> {
-        for i in items {
-            if let BookItem::Chapter(c) = i {
-                if c.path == Some(std::path::PathBuf::from("backends/intro.md")) {
-                    return Some(c);
-                } else if let Some(c) = Self::find_languages_item(&mut c.sub_items) {
-                    return Some(c);
-                }
-            }
-        }
-        None
-    }
-
-    fn generate_language_supports(language: &str, chapter: &mut Chapter) {
+    fn generate_language_supports(language: &str) -> String {
         #[derive(Template)]
         #[template(path="supports.md.jinja")]
         struct SupportsBlock {
             attr_support : BackendAttrSupport,
         }
-        chapter.content.push('\n');
-        chapter.content.push_str(&SupportsBlock { attr_support: diplomat_tool::get_supported(language) }.render().expect("Could not render supports block."));
+        SupportsBlock { attr_support: diplomat_tool::get_supported(language) }.render().expect("Could not render supports block.")
     }
 }
 
@@ -71,36 +57,13 @@ impl Preprocessor for DiplomatPreprocessor {
         _ctx: &mdbook_preprocessor::PreprocessorContext,
         mut book: mdbook_preprocessor::book::Book,
     ) -> mdbook_preprocessor::errors::Result<mdbook_preprocessor::book::Book> {
-        let mut languages = vec!["c", "cpp", "dart", "demo_gen", "js", "kotlin", "nanobind"];
-
-        let book_item: &mut Chapter = Self::find_languages_item(&mut book.items).unwrap();
-        for i in &mut book_item.sub_items {
-            if let BookItem::Chapter(c) = i {
-                if let Some(p) = c.path.clone() {
-                    let name = p.file_stem().unwrap().to_str().unwrap();
-                    let language = languages.iter().position(|i| *i == name.to_lowercase());
-                    if let Some(l) = language {
-                        languages.remove(l);
-                        Self::generate_language_supports(name, c);
-                    }
-                }
-            }
-        }
-        // Create new pages:
-        for l in languages {
-            let mut nm = book_item.number.clone().unwrap();
-            nm.push(book_item.sub_items.len() as u32);
-            let mut ch = Chapter {
-                name: l.to_string(),
-                parent_names: vec![book_item.name.clone()],
-                path: Some(format!("backends/{l}").into()),
-                number: Some(nm),
-                content: format!("# {l}"),
-                ..Default::default()
-            };
-            Self::generate_language_supports(l, &mut ch);
-            book_item.sub_items.push(BookItem::Chapter(ch));
-        }
+        let mut env = Environment::new();
+        env.add_function("supports", Self::generate_language_supports);
+        // Evaluate each page as an askama template:
+        book.for_each_chapter_mut(|ch| {
+            let expr = env.template_from_named_str(&ch.name, &ch.content).expect("Could not compile book expression.");
+            ch.content = expr.render(context! {}).expect("Could not render template.");
+        });
         Ok(book)
     }
 }
