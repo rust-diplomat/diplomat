0035280eebabe2f7a902181b0b77abb3839512ca
tyler-zeromatter <tyler-zeromatter@users.noreply.github.com>
2026-05-06 23:24:44 +0000

    deploy: 128553d523e44695725dc556e68a987b5224cbc7

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-79822b30.js',
+                path_to_root + 'searchindex-50536f91.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
