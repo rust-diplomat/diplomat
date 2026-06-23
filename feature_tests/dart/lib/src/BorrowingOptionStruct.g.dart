9bf445e732513ec77cf930f8bf1909af75609eaf
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-06-23 13:25:31 +0200

    dart: null-aware field syntax

--- feature_tests/dart/lib/src/BorrowingOptionStruct.g.dart
+++ feature_tests/dart/lib/src/BorrowingOptionStruct.g.dart
@@ -47,7 +47,7 @@ final class BorrowingOptionStruct {
   // assuming that there are no `'other: a`. bounds. In case of such bounds,
   // the caller should take care to also call _fieldsForLifetimeOther
   // ignore: unused_element
-  core.List<Object> get _fieldsForLifetimeA => [if (a != null) a!];
+  core.List<Object> get _fieldsForLifetimeA => [?a];
 }
 
 // dart format on
