2422bb4e625b5eee28f29d01eb69845de8e94d3c
Moritz <mosum@google.com>
2026-07-16 09:24:14 +0000

    feat(hir): introduce single bug attribute tag for unrecoverable errors

--- core/src/hir/attrs.rs
+++ core/src/hir/attrs.rs
@@ -71,8 +71,6 @@ pub struct Attrs {
 
     /// From #[diplomat::attr()]. Mark an error type as an unrecoverable bug/precondition failure (Error in Dart/panic).
     pub bug: bool,
-    /// From #[diplomat::attr()]. Mark an error type as a catchable runtime failure (Exception in Dart/catchable).
-    pub catchable: bool,
 
     /// Information on if a type declaration/impl block has custom bindings, and if so, what kind.
     pub custom_extra_code: HashMap<IncludeLocation, IncludeSource>,
@@ -606,9 +604,6 @@ impl Attrs {
                         "bug" => {
                             this.bug = true;
                         }
-                        "catchable" => {
-                            this.catchable = true;
-                        }
                         "custom_extra_code" => {
                             let (location, source) =
                                 IncludeLocation::pair_from_meta(&attr.meta, errors);
@@ -763,7 +758,6 @@ impl Attrs {
             mut_struct_ref,
             tuple,
             bug: _,
-            catchable: _,
             custom_extra_code,
             default_value,
         } = &self;
@@ -1248,7 +1242,6 @@ impl Attrs {
             // Not inherited
             tuple: false,
             bug: false,
-            catchable: false,
             // Not inherited
             custom_extra_code: Default::default(),
             // Not inherited
