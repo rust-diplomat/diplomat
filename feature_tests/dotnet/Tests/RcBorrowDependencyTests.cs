12b61d5221e35de92ff7df66e11b7b8c7b332c9b
Junyi Ou <jou@devolutions.net>
2026-09-17 11:56:04 -0400

    dotnet: centralize lifetime test GC helpers

--- feature_tests/dotnet/Tests/RcBorrowDependencyTests.cs
+++ feature_tests/dotnet/Tests/RcBorrowDependencyTests.cs
@@ -5,37 +5,6 @@ using Xunit;
 
 namespace Somelib.FeatureTests;
 
-// The native drop counters are shared across tests.
-[CollectionDefinition(Name, DisableParallelization = true)]
-public class RcSharedNativeStateCollection
-{
-    public const string Name = "RcSharedNativeState";
-}
-
-internal static class TestGc
-{
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    internal static void DrainFinalizers()
-    {
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
-        GC.Collect();
-    }
-
-    [MethodImpl(MethodImplOptions.NoInlining
-#if !NETFRAMEWORK
-        | MethodImplOptions.AggressiveOptimization
-#endif
-    )]
-    internal static void ForceUntil(Func<bool> condition)
-    {
-        for (int i = 0; i < 50 && !condition(); i++)
-        {
-            DrainFinalizers();
-        }
-    }
-}
-
 [Collection(RcSharedNativeStateCollection.Name)]
 public class RcBorrowDependencyTests
 {
